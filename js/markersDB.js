d5144b010b064f59ea1dadc3452f79f0f1b0c232
IsItNevis <94104284+IsNevis@users.noreply.github.com>
2022-01-15 04:12:27 +0530

    Understood how markers.js and markersDB works and configured them a litte to fit isles. Only a few things were changed(more are there to do) and icons were also not changed

--- js/markersDB.js
+++ js/markersDB.js
@@ -4,8 +4,8 @@ xhttp.onreadystatechange = function() {
     var locations = JSON.parse(xhttp.responseText).locations;
     for(var a in locations) {
       var loc = locations[a];
-      for(var i in markers.normalrender) {
-        var m = markers.normalrender[i];
+      for(var i in markers.Isles) {
+        var m = markers.Isles[i];
         if(m.icon == loc.icon) {
           if(markersDB[m.groupName]) {
             markersDB[m.groupName].raw.push({
@@ -28,334 +28,105 @@ xhttp.open('GET', 'https://raw.githubusercontent.com/IsNevis/EnhancedIslesMap/ma
 xhttp.send();
 
 var markersDB = {
-  "Armours": {
-    "created": true,
-    "name": "Armours",
-    "raw": []
+  // groupname is used to create a link in markerDB For Example "Armours" : {} is used to create a link in markerDB
+  // If icon in database =icon in markers.js then it gets grouped in the groupname
+  // Display names dont serve any purpose
+  // set createInfoWindow to false just incase
+  // checked should be true if you want it displayed in map load
+
+  "Peaceful Quests": {
+    created: true,
+    name: "Peaceful Quests",
+    raw: [],
+  },
+  "Easy Quests": {
+    created: true,
+    name: "Easy Quests",
+    raw: [],
+  },
+  "Normal Quests": {
+    created: true,
+    name: "Normal Quests",
+    raw: [],
+  },
+  "Hard Quests": {
+    created: true,
+    name: "Hard Quests",
+    raw: [],
+  },
+  "Hardcore Quests": {
+    created: true,
+    name: "Hardcore Quests",
+    raw: [],
+  },
+  "Shortcuts": {
+    created: true,
+    name: "Shortcuts",
+    raw: [],
+  },
+  "Cave": {
+    created: true,
+    name: "Cave",
+    raw: [],
+  },
+  "General Store": {
+    created: true,
+    name: "General Store",
+    raw: [],
+  },
+  "Boat Merchants": {
+    created: true,
+    name: "Boat Merchants",
+    raw: [],
+  },
+  "Marketeer": {
+    created: true,
+    name: "Marketeer",
+    raw: [],
+  },
+  "Alchemy Station": {
+    created: true,
+    name: "Alchemy Station",
+    raw: [],
+  },
+  "Armouring Station": {
+    created: true,
+    name: "Armouring Station",
+    raw: [],
+  },
+  "Cooking Station": {
+    created: true,
+    name: "Cooking Station",
+    raw: [],
+  },
+  "Jeweling Station": {
+    created: true,
+    name: "Jeweling Station",
+    raw: [],
+  },
+  "Weaponsmithing Station": {
+    created: true,
+    name: "Weaponsmithing Station",
+    raw: [],
+  },
+  "Woodworking Station": {
+    created: true,
+    name: "Woodworking Station",
+    raw: [],
+  },
+  "Altars": {
+    created: true,
+    name: "Altars",
+    raw: [],
+  },
+  "Boss": {
+    created: true,
+    name: "Boss",
+    raw: [],
   },
   "Banks": {
-    "created": true,
-    "name": "Banks",
-    "raw": []
-  },
-  "BoatMerchants": {
-    "created": true,
-    "name": "BoatMerchants",
-    "raw": []
-  },
-  "BookStores": {
-    "created": true,
-    "name": "BookStores",
-    "raw": []
-  },
-  "BowlMerchant": {
-    "created": true,
-    "name": "BowlMerchant",
-    "raw": []
-  },
-  "BucketMerchant": {
-    "created": true,
-    "name": "BucketMerchant",
-    "raw": []
-  },
-  "Dungeons": {
-    "created": true,
-    "name": "Dungeons",
-    "raw": []
-  },
-  "EggMerchant": {
-    "created": true,
-    "name": "EggMerchant",
-    "raw": []
-  },
-  "FishMerchant": {
-    "created": true,
-    "name": "FishMerchant",
-    "raw": []
-  },
-  "HorseMerchant": {
-    "created": true,
-    "name": "HorseMerchant",
-    "raw": []
-  },
-  "Identifier": {
-    "created": true,
-    "name": "Identifier",
-    "raw": []
-  },
-  "ItemBuyer": {
-    "created": true,
-    "name": "Item Buyer",
-    "raw": []
-  },
-  "JunkMerchants": {
-    "created": true,
-    "name": "JunkMerchants",
-    "raw": []
-  },
-  "KeyMerchant": {
-    "created": true,
-    "name": "KeyMerchant",
-    "raw": []
-  },
-  "LiquidMerchant": {
-    "created": true,
-    "name": "Identifier",
-    "raw": []
-  },
-  "MushroomMerchant": {
-    "created": true,
-    "name": "MushroomMerchant",
-    "raw": []
-  },
-  "NetherPortal": {
-    "created": true,
-    "name": "NetherPortal",
-    "raw": []
-  },
-  "PotatoMerchant": {
-    "created": true,
-    "name": "PotatoMerchant",
-    "raw": []
-  },
-  "Potions": {
-    "created": true,
-    "name": "Potions",
-    "raw": []
-  },
-  "Quests": {
-    "created": true,
-    "name": "Quests",
-    "raw": []
-  },
-  "ScrollMerchant": {
-    "created": true,
-    "name": "ScrollMerchant",
-    "raw": []
-  },
-  "Weapons": {
-    "created": true,
-    "name": "Weapons",
-    "raw": []
-  },
-  "WheatMerchant": {
-    "created": true,
-    "name": "WheatMerchant",
-    "raw": []
-  },
-  "WoolMerchant": {
-    "created": true,
-    "name": "WoolMerchant",
-    "raw": []
-  },
-  "AccessoryMerchant": {
-    "created": true,
-    "name": "AccessoryMerchant",
-    "raw": []
-  },
-  "WaterBottleMerchant": {
-    "created": true,
-    "name": "WaterBottleMerchant",
-    "raw": []
-  },
-  "Seasail Merchant": {
-    "created": true,
-    "name": "Seasail Merchant",
-    "raw": []
-  },
-  "Cobblestone Merchant": {
-    "created": true,
-    "name": "Cobblestone Merchant",
-    "raw": []
-  },
-  "Melon Merchant": {
-    "created": true,
-    "name": "Melon Merchant",
-    "raw": []
-  },
-  "Explosives Merchant": {
-    "created": true,
-    "name": "Explosives Merchant",
-    "raw": []
-  },
-  "Fabrics Merchant": {
-    "created": true,
-    "name": "Fabicrs Merchant",
-    "raw": []
-  },
-  "Food Merchant": {
-    "created": true,
-    "name": "Food Merchant",
-    "raw": []
-   },
-  "Rymek Dealer Merchant": {
-    "created": true,
-    "name": "Rymek Dealer Merchant",
-    "raw": []
-   },
-  "Feather Merchant": {
-    "created": true,
-    "name": "Feather Merchant",
-    "raw": []
-   },
-  "Exotic Potions Merchant": {
-    "created": true,
-    "name": "Exotic Potions Merchant",
-    "raw": []
-   },
-  "Art Merchant": {
-    "created": true,
-    "name": "Art Merchant",
-    "raw": []
-   },
-  "Scam Merchant": {
-    "created": true,
-    "name": "Scam Merchnat",
-    "raw": []
-   },
-  "Shears Merchant": {
-    "created": true,
-    "name": "Shears Merchnat",
-    "raw": []
-   },
-  "Powder Master": {
-    "created": true,
-    "name": "Powder Master",
-    "raw": []
-   },
-  "Powder Merchant": {
-	"created": true,
-	"name": "Powder Merchant",
-	"raw": []
-   },
-  "Quartz Merchant": {
-    "created": true,
-    "name": "Quartz Merchant",
-    "raw": []
-   },
-  "Artefact Merchant": {
-    "created": true,
-    "name": "Artefact Merchant",
-    "raw": []
-   },
-  "Dungeon Merchant": {
-	"created": true,
-	"name": "Dungeon Merchant",
-	"raw": []
-   },
-  "Fast Travel": {
-	"created": true,
-	"name": "Fast Travel",
-	"raw": []
-   },
-  "Uth Shrine": {
-	"created": true,
-	"name": "Uth Shrine",
-	"raw": []
-   },
-  "Ultimate Discovery": {
-	"created": true,
-	"name": "Ultimate Discovery",
-	"raw": []
-   },
-  "Light's Realm": {
-	"created": true,
-	"name": "Light Realm Entrance",
-	"raw": []
-   },
-  "Key Forge Merchant": {
-	"created": true,
-	"name": "Key Forge Merchant",
-	"raw": []
-   },
-  "Corrupted Dungeons": {
-	"created": true,
-	"name": "Corrupted Dungeons",
-	"raw": []
-   },
-  "Guild Master": {
-	"created": true,
-	"name": "Guild Master",
-	"raw": []
-   },
-   "Grind Spot": {
-	"created": true,
-	"name": "Grind Spot",
-	"raw": []
-   },
-   "Cave": {
-	"created": true,
-	"name": "Cave",
-	"raw": []
-   },
-   "Mini Quest": {
-	"created": true,
-	"name": "Mini Quest",
-	"raw": []
-   },
-   "Trade Market": {
-	"created": true,
-	"name": "Trade Market",
-	"raw": []
-   },
-   "Alchemism Station": {
-	"created": true,
-	"name": "Alchemism Station",
-	"raw": []
-   },
-   "Armouring Station": {
-	"created": true,
-	"name": "Armouring Station",
-	"raw": []
-   },
-   "Cooking Station": {
-	"created": true,
-	"name": "Cooking Station",
-	"raw": []
-   },
-   "Jeweling Station": {
-	"created": true,
-	"name": "Jeweling Station",
-	"raw": []
-   },
-   "Scribing Station": {
-	"created": true,
-	"name": "Scribing Station",
-	"raw": []
-   },
-   "Tailoring Station": {
-	"created": true,
-	"name": "Tailoring Station",
-	"raw": []
-   },
-   "Weaponsmithing Station": {
-	"created": true,
-	"name": "Weaponsmithing Station",
-	"raw": []
-   },
-   "Woodworking Station": {
-	"created": true,
-	"name": "Woodworking Station",
-	"raw": []
-   },
-   "Tool Merchant": {
-	"created": true,
-	"name": "Tool Merchant",
-	"raw": []
-   },
-   "Boss Altar": {
-	"created": true,
-	"name": "Boss Altar",
-	"raw": []
-   },
-   "Dungeon Merchant": {
-	"created": true,
-	"name": "Dungeon Merchant",
-	"raw": []
-   },
-  "Raids": {
-    "created": true,
-    "name": "Raids",
-    "raw": []
+    created: true,
+    name: "Banks",
+    raw: [],
   },
 };
